174a221b8b038ef99e419ac4b45898d7b1f594e0
MING9UCCI <64111159+MING9UCCI@users.noreply.github.com>
2024-03-23 23:40:29 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,7 +1,7 @@
 def solution(a, b, c):
-    if a != b and b != c and a != c:
-        return a+b+c
-    elif a == b != c or a != b == c or b != c == a:
-        return (a+b+c)*(a**2+b**2+c**2)
+    if a == b == c:
+        return (a+b+c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    elif a==b or a==c or b==c:
+        return (a+b+c) * (a**2 + b**2 + c**2)
     else:
-        return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
+        return (a+b+c)
