b91e9012450fb895f0d8e97d269fd9af7b82e912
MING9UCCI <64111159+MING9UCCI@users.noreply.github.com>
2024-04-01 23:10:08 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 8.77 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,13 +1,13 @@
 def solution(arr, queries):
-    result = [] #결과 제출을 위한 result 배열 생성
-    for i in range(len(queries)): #queries의 값 만큼 반복, len(queries) = len(result)
-        small = 1000000
-        for j in range(queries[i][0], queries[i][1]+1, 1): #0 ~ 4까지 반복
-            if arr[j] > queries[i][2]: #만약 j가 2보다 크다면
-                if small > arr[j]:
-                    small = arr[j]
-        if small == 1000000:
+    result = []
+    for i in range(len(queries)):
+        min = 1000000
+        for j in range(queries[i][0], queries[i][1]+1, 1):
+            if arr[j] > queries[i][2]:
+                if min > arr[j]:
+                    min = arr[j]
+        if min == 1000000:
             result.append(-1)
         else:
-            result.append(small)
+            result.append(min)
     return result
