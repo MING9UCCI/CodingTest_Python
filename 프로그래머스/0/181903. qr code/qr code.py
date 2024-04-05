30689fbd3c2022db0f6c128b047150e5de4ef679
Minwoo <64111159+MING9UCCI@users.noreply.github.com>
2024-04-05 23:16:21 +0900

    [level 0] Title: qr code, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.py
+++ 프로그래머스/0/181903. qr code/qr code.py
@@ -4,4 +4,4 @@ def solution(q, r, code):
         if idx % q == r: answer += val
     return answer
 
-# 어제 다른 사람의 코드에서 본 'enumerate'를 이용하여 내 나름 숏코딩을 해 보았다.
+# 어제 다른 사람의 코드에서 본 'enumerate'가 기억나서, 이를 이용하여 내 나름의 숏코딩을 해 보았다.
