b7cc7417d127025cc810c81d1701d1f457bd44ba
Minwoo <64111159+MING9UCCI@users.noreply.github.com>
2024-04-05 22:37:06 +0900

    [level 0] Title: 접미사 배열, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -9,3 +9,13 @@ def solution(my_string):
         queue.popleft()
     answer.sort()
     return answer
+
+
+# def solution(my_string):
+#     answer = []
+#     for i in range(len(my_string)):
+#         answer.append(my_string[-i:])
+#     answer.sort()
+#     return answer
+
+#사실 이렇게 간단하게 짜고 싶었지만, queue를 사용해 보고 싶었다.
