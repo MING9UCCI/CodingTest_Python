389f4c21e94b2b1a58bc6f927d03e75392e07954
Minwoo <64111159+MING9UCCI@users.noreply.github.com>
2024-04-05 23:03:59 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -14,3 +14,8 @@ def solution(my_string, s, e):
         else:
             answer += my_string[i]
     return answer
+
+
+# 다른 사람의 풀이에서 이런 코드 보고 '아하 이런 방법이 있었다니'라고 생각함
+# def solution(my_string, s, e):
+#     return my_string[:s]+my_string[s:e+1][::-1]+my_string[e+1:]
