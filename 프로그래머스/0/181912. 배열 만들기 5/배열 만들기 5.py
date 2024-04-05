30f275dfd5c99cbd12001b9a7012793050ceaa03
Minwoo <64111159+MING9UCCI@users.noreply.github.com>
2024-04-05 22:23:03 +0900

    [level 0] Title: 배열 만들기 5, Time: 3.85 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -10,3 +10,15 @@ def solution(intStrs, k, s, l):
         else:
             temp = ""
     return answer
+
+# 내가 보기에 더 나은 코드. 참고.
+# def solution(intStrs, k, s, l):
+#     answer = []
+
+#     for intStr in intStrs:
+#         substr = intStr[s:s+l]
+#         num = int(substr)
+#         if num > k:
+#             answer.append(num)
+
+#     return answer
