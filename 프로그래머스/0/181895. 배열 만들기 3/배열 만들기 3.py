ed27329d6f0f8e64980b3706a57843528241786a
Minwoo <64111159+MING9UCCI@users.noreply.github.com>
2024-04-08 23:28:06 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.54 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -4,3 +4,10 @@ def solution(arr, intervals):
         for j in range(intervals[i][0], intervals[i][1]+1):
             answer.append(arr[j])
     return answer
+
+
+# 다른 사람들의 코드를 보고 느낀 효율적인 모범 답안.
+# def solution(arr, intervals):
+#     s1, e1 = intervals[0]
+#     s2, e2 = intervals[1]
+#     return arr[s1:e1+1] + arr[s2:e2+1]
