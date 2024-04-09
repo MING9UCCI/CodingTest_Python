90261698bc64198010ba20ae99c6c5c060c87021
Minwoo <64111159+MING9UCCI@users.noreply.github.com>
2024-04-09 22:15:55 +0900

    [level 0] Title: 배열 조각하기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -1,15 +1,6 @@
-# def solution(arr, query):
-#     for idx, num in enumerate(query):
-#         if num % 2 == 0:
-#             arr = arr[:num+1]
-#         else:
-#             arr = arr[num:]
-#     return arr
-
-
 def solution(arr, query):
-    for i, query in enumerate(query):
-        if i % 2 == 0:
+    for idx, query in enumerate(query):
+        if idx % 2 == 0:
             arr = arr[:query+1]
         else:
             arr = arr[query:]
