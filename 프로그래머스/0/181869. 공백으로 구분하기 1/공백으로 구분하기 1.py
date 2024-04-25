50367a688f0ce960b31b1e18248c50d03ddc2fa4
Minwoo <64111159+MING9UCCI@users.noreply.github.com>
2024-04-25 23:39:30 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.py
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.py
@@ -9,3 +9,7 @@ def solution(my_string):
             string += s
     result.append(string)
     return result
+
+# def solution(my_string):
+#     return my_string.split(' ')
+# 이 간단한 방법을 왜 생각 못했을까 ㅎㅎ;;
